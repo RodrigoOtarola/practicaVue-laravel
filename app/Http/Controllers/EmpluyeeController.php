229d4c21e4df036c9edfb7e0b1ec3be877d5c45d
RodrigoOtarola <rodrigo.otarola.gutierrez@ciisa.cl>
2023-12-23 22:25:19 -0300

    create project

---
+++ app/Http/Controllers/EmpluyeeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Empluyee;
+use Illuminate\Http\Request;
+
+class EmpluyeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Empluyee $empluyee)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Empluyee $empluyee)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Empluyee $empluyee)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Empluyee $empluyee)
+    {
+        //
+    }
+}
